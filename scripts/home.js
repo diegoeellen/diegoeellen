dfbfad885a6b3e88d1c79ada0811f66960577a32
Diego Pereira <dgo.andrade88@gmail.com>
2024-04-03 21:45:50 +0100

    Update home.js

--- scripts/home.js
+++ scripts/home.js
@@ -5,25 +5,25 @@ $(document).ready(() => {
 
 function countDown() {
     // Set the date we're counting down to
-    var countDownDate = new Date("Jan 4, 2020 11:00:00").getTime();
+    let countDownDate = new Date("Jan 4, 2020 11:00:00").getTime();
 
     // Update the count down every 1 second
     var x = setInterval(function () {
 
         // Get todays date and time
-        var now = new Date().getTime();
-
-        // Find the distance between now and the count down date
-        var distance = countDownDate - now;
-        if(distance < 0) {
-            console.log('menor');
+        let now = new Date().getTime();        
+        let distance = countDownDate - now;        
+        let days = 0;
+        let hours = 0;
+        let minutes = 0;
+        let seconds = 0;
+        
+        if (distance > 0) {
+            days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            seconds = Math.floor((distance % (1000 * 60)) / 1000);
         }
-
-        // Time calculations for days, hours, minutes and seconds
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
         document.getElementById("cd-days").innerHTML = days;
         document.getElementById("cd-hours").innerHTML = hours;
@@ -33,7 +33,7 @@ function countDown() {
         // If the count down is finished, write some text 
         if (distance < 0) {
             clearInterval(x);
-            document.getElementById("demo").innerHTML = "CASADOS!!";
+            document.querySelector("#countdown").previousElementSibling.innerHTML = "ENFIM CASADOS s2 s2 s2"
         }
     }, 1000);
 }
